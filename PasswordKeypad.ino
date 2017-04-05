fb00baf75b8058587cb2c679252ecd1bad922e16
rudeldi <rudie@dhcp229-200.wlan.rz.tu-bs.de>
2017-04-05 13:42:25 +0200

    Checkpoints

--- PasswordKeypad.ino
+++ PasswordKeypad.ino
@@ -7,8 +7,10 @@
 
 #define _myArray_cnt 4
 
-int check = 1; //Variable um Keypad zu deaktivieren, 0 = aus
-int check_2 = 0; //Variable um Stangenspiel zu aktivieren, 0 = aus
+unsigned int check = 1; //Variable um Keypad zu aktivieren, 0 = aus
+unsigned int check_2 = 0; //Variable um Stangenspiel zu aktivieren, 0 = aus
+unsigned int check_3 = 0; //Variable um Station 3 zu aktivieren, 0 = aus
+unsigned int check_4 = 0; //Variable um Station 4 zu aktivieren, 0 = aus
 
 byte ledPin = 13;
 
@@ -162,6 +164,7 @@ void keypadEvent(KeypadEvent key) {
         else {
           Serial.println("You have to press more numbers");
           LCDmoreNumbers();
+          break;
 
         }
       }
